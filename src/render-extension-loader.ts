efac58e4c49f207493d584958e05224828573f29
Guilherme Rodrigues <gadr90@gmail.com>
2018-03-01 18:01:00 -0300

    Remove global runtime and use private variable

--- src/render-extension-loader.ts
+++ src/render-extension-loader.ts
@@ -1,5 +1,6 @@
 interface Window {
   __RENDER_6_RUNTIME__: any,
+  __RENDER_7_RUNTIME__: any,
   __RUNTIME__: any,
   $: any,
   RenderExtensionLoader: any,
@@ -12,6 +13,8 @@ class RenderExtensionLoader {
   private locale: string
   private env: string
   private verbose: any
+  private renderMajor: number
+  private runtime: any
   private get: any
   private styles: string[]
   private scripts: string[]
@@ -23,14 +26,22 @@ class RenderExtensionLoader {
     this.locale = locale || 'en-US'
     this.verbose = verbose
     this.env = env || /myvtexdev\.com/.test(window.location.hostname) ? 'myvtexdev' : 'myvtex'
-    this.get = (window.$ && window.$.get) || window.fetch && ((url) => window.fetch(url).then(res => res.json()))
-
+    this.get = window.$
+      ? ((url) => window.$.ajax({url}))
+      : window.fetch
+        ? ((url) => window.fetch(url).then(res => res.json()))
+        : null
+    
+    if (!window.__RUNTIME__) {
+      window.__RUNTIME__ = {account, workspace}
+    }
+    
     if (!this.get) {
       throw new Error('Render Extension Loader requires either jQuery.ajax or window.fetch.')
     }
   }
 
-  public async fetch() {
+  public fetch = async () => {
     if (this.verbose) {
       console.time('render-extension-loader:json')
     }
@@ -38,9 +49,10 @@ class RenderExtensionLoader {
     if (this.verbose) {
       console.timeEnd('render-extension-loader:json')
     }
+    this.renderMajor = runtime.renderMajor || 6
     this.styles = styles
     this.scripts = scripts
-    window.__RUNTIME__ = {
+    this.runtime = {
       ...runtime,
       start: false,
     }
@@ -57,22 +69,44 @@ class RenderExtensionLoader {
     return Object.keys(runtime.extensions)
   }
 
-  public render(extension, props, element) {
-    window.__RUNTIME__.extensions[extension].props = {
-      ...window.__RUNTIME__.extensions[extension].props,
-      ...props,
+  public render = (extension, element, props) => {
+    if (props) {
+      this.runtime.extensions[extension].props = {
+        ...this.runtime.extensions[extension].props,
+        ...props,
+      }
     }
+
     if (this.verbose) {
       console.time('render-extension-loader:render')
     }
-    window.__RENDER_6_RUNTIME__.render(extension, element)
+
+    const runtime = window[`__RENDER_${this.renderMajor}_RUNTIME__`]
+    runtime.render(extension, this.runtime, element)
+    
     if (this.verbose) {
       console.timeEnd('render-extension-loader:render')
     }
   }
 
-  private addScriptToPage(src) {
+  private getExistingScriptSrcs = () => {
+    const paths = []
+    for (let i = 0; i < document.scripts.length; i++) {
+      paths.push(document.scripts.item(i).src)
+    }
+    return paths
+  }
+
+  private scriptOnPage = (path) => {
+    return this.getExistingScriptSrcs().some(src => src.indexOf(path) !== -1)
+  }
+
+  private addScriptToPage = (src) => {
     return new Promise((resolve, reject) => {
+      if (this.scriptOnPage(src)) {
+        return resolve()
+      }
+
       const script = document.createElement('script')
       script.src = src
       script.onload = resolve
@@ -82,7 +116,7 @@ class RenderExtensionLoader {
     })
   }
 
-  private addStyleToPage(href) {
+  private addStyleToPage = (href) => {
     const link = document.createElement('link')
     link.href = href
     link.type = 'text/css'
