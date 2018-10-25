8cc379e89da19e9ea34da61d06c45b3f404fa2cc
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-10-25 14:07:04 -0300

    Create .min.js output

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,24 @@ import typescript from 'rollup-plugin-typescript2'
 import uglify from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
+const addMinExtension = (filePath) => {
+  return filePath.replace(/\.([^.]*)$/, '.min.$1')
+}
+
 export default [
   {
     input: 'src/render-extension-loader.ts',
-    output: {
+    output: [{
       name: 'RenderExtensionLoader',
       file: pkg.browser,
       format: 'umd',
       sourcemap: true,
-    },
+    }, {
+      name: 'RenderExtensionLoader',
+      file: addMinExtension(pkg.browser),
+      format: 'umd',
+      sourcemap: true,
+    }],
     plugins: [
       typescript(),
       uglify(),
